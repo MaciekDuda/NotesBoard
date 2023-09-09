b1153f5ae7a2b6d3010968423c8bf373ac8880da
MaciekDuda <maciejduda1626@gmail.com>
2023-09-09 14:15:14 +0200

    creating note feature

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const cancelBtn = document.querySelector('.cancel');
 const deleteAllBtn = document.getElementsByClassName('.delete-all');
 const deleteBtn = document.getElementsByClassName('.delete-note');
 
-
 const noteArea = document.querySelector('.note-area');
 const notePanel = document.querySelector('.note-panel');
 const category = document.querySelector('#category');
@@ -14,30 +13,76 @@ const error = document.querySelector('.error');
 let selectedValue;
 let cardID = 0;
 
-const openPanel = () => { 
-    notePanel.style.display = 'flex';
-    error.style.visbility = 'hidden';
+const noteCleaner = () => { 
     textArea.value = '';
-    category.selectedIndex = 0;
+	category.selectedIndex = 0;
+    notePanel.style.display = 'none';
  }
 
-const closePanel = () => { 
-    notePanel.style.display = 'none'
+const openPanel = () => {
+	notePanel.style.display = 'flex';
+};
 
+const closePanel = () => {
+	error.style.visibility = 'hidden';
+    noteCleaner()
+};
+
+const createNote = () => {
+	const newNote = document.createElement('div');
+	newNote.classList.add('note');
+	newNote.setAttribute('id', cardID);
+
+	newNote.innerHTML = `<div class="note-header">
+    <h3 class="note-title">${selectedValue} #${cardID+1}</h3>
+    <button class="delete-note">
+        <i class="fa-solid fa-circle-xmark"></i>
+    </button>
+</div>
+<div class="note-body">
+    ${textArea.value}
+</div>`;
+
+	noteArea.appendChild(newNote);
+	cardID++;
+
+    noteCleaner()
+};
+
+const selectValue = () => { 
+    selectedValue = category.options[category.selectedIndex].text;
+    console.log(selectedValue);
  }
 
- const createNote = (second) => { third }
+const addNote = () => {
+	if (
+		textArea.value !== '' &&
+		category.options[category.selectedIndex].value !== '0'
+	) {
+		createNote();
+		error.style.visibility = 'hidden';
+	} else {
+		error.style.visibility = 'visible';
+	}
+};
 
- const addNote = () => { 
-    if(textArea.value !== '' && category.options[category.selectedIndex].value !== '0') {
-        error.style.visbility = 'hidden'
-    } else {
-        error.style.visibility = 'visible'
-    }
-  }
+addBtn.addEventListener('click', openPanel);
+cancelBtn.addEventListener('click', closePanel);
+saveBtn.addEventListener('click', addNote);
 
 
+//playground
 
- addBtn.addEventListener('click', openPanel);
- cancelBtn.addEventListener('click', closePanel);
- saveBtn.addEventListener('click', addNote);
+document.getElementById('czasButton').addEventListener('click', () => {
+    const teraz = new Date();
+    const dzien = teraz.getDate();
+    const miesiac = teraz.getMonth() + 1; // Miesiące są numerowane od 0 do 11, dlatego dodajemy 1
+    const rok = teraz.getFullYear();
+    const godzina = teraz.getHours();
+    const minuta = teraz.getMinutes();
+
+    // Formatowanie czasu w formacie "dzień-miesiąc-rok godzina:minuta"
+    const czas = `${dzien}-${miesiac}-${rok} ${godzina}:${minuta}`;
+
+    document.getElementById('czasP').textContent = czas;
+});
